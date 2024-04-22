1823dae44a3a9ba28ce8aa3a12f976de84d1eb36
Ogame3334 <kazuhiroisobe0522@gmail.com>
2024-04-22 11:08:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# おがめ用 discord bot
+## 手順
+```bash
+# 初回のみ
+cd docker
+docker build -t dpp:latest .
+cd ..
+# 
+
+docker compose up -d
+```
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <dpp/dpp.h>
+
+int main(void){
+    std::cout << "Hello, world!" << std::endl;
+    dpp::cluster bot{""};
+
+    return 0;
+}
